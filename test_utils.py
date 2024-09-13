2e13199f128d925da2a9dbcb5d26485be1c45d90
declanyg <declan@youngs.asia>
2024-09-13 11:54:52 -0400

    Finished test_utils and utils

--- test_utils.py
+++ test_utils.py
@@ -14,18 +14,28 @@ def test_dot_product():
     
 def test_cosine_similarity():
     ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([1, 2, 3])
+
+    result = cosine_similarity(vector1, vector2)
     
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
+    expected_result = (1*1+2*2+3*3)/((1**2+2**2+3**2)**0.5*(1**2+2**2+3**2)**0.5)
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
     ### YOUR CODE HERE
+    target_vector = np.array([1, 2, 3])
+    matrix = np.array([[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9]])
     
-    result = ### YOUR CODE HERE
+    result = nearest_neighbor(target_vector, matrix)
     
-    expected_index = ### YOUR CODE HERE
+    expected_index = 0
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
+
+test_dot_product()
+test_cosine_similarity()
+test_nearest_neighbor()
